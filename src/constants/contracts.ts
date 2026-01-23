e32a1cab71fcb575cfe048e9b2716a65e869acd0
Mauro Eijsenring <mauro@eijsenring.com>
2026-01-23 12:25:07 +0100

    add StAztec abi

--- src/constants/contracts.ts
+++ src/constants/contracts.ts
@@ -1,5 +1,6 @@
 import OllaCoreABI from "../generated/abis/OllaCore.json";
 import MockAztecABI from "../generated/abis/MockAztec.json";
+import StAztecABI from "../generated/abis/StAztec.json";
 import addresses from "../generated/deployments/addresses.json";
 
 export const CONTRACTS = {
@@ -10,5 +11,9 @@ export const CONTRACTS = {
   OllaCore: {
     address: addresses.OllaCoreProxy as `0x${string}`,
     abi: OllaCoreABI,
+  },
+  StAztec: {
+    addresses: addresses.StAztec as `0x${string}`,
+    abi: StAztecABI,
   },
 } as const;
