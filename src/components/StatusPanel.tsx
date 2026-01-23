67af6a24c4693ee147396bc847ab6d2cfca79b70
Mauro Eijsenring <mauro@eijsenring.com>
2026-01-23 14:23:39 +0100

    small change in ui to use the stAztec hook to get the balance

--- src/components/StatusPanel.tsx
+++ src/components/StatusPanel.tsx
@@ -1,9 +1,14 @@
 interface StatusPanelProps {
+  stAztecBalance: string;
   balance: string;
   allowance: string;
 }
 
-export function StatusPanel({ balance, allowance }: StatusPanelProps) {
+export function StatusPanel({
+  stAztecBalance,
+  balance,
+  allowance,
+}: StatusPanelProps) {
   return (
     <div className="bg-blue-50 p-4 rounded-xl border border-blue-100 space-y-2">
       <h2 className="text-sm font-semibold text-blue-800 uppercase tracking-wide">
@@ -12,6 +17,10 @@ export function StatusPanel({ balance, allowance }: StatusPanelProps) {
       <div className="text-sm text-gray-700 flex justify-between">
         <span>Asset Balance:</span>
         <span className="font-mono">{balance} AZT</span>
+      </div>
+      <div className="text-sm text-gray-700 flex justify-between">
+        <span>stAztec Balance:</span>
+        <span className="font-mono">{stAztecBalance} stAZT</span>
       </div>
       <div className="text-sm text-gray-700 flex justify-between">
         <span>Allowance:</span>
