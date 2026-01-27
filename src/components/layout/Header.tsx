1d5b32c5130600bbb387a3ab1a8addaa49d47464
Mauro Eijsenring <mauro@eijsenring.com>
2026-01-27 13:44:02 +0100

    added the logo to the frontend

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,11 +1,10 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
+import ollaLogo from "@/assets/logo/olla-logo-black.svg";
 
 export function Header() {
   return (
     <header className="flex justify-between items-center mb-8 border-b border-gray-100 pb-4">
-      <h1 className="text-2xl font-bold text-gray-800 tracking-tight">
-        Olla Dashboard
-      </h1>
+      <img src={ollaLogo} alt="Olla" className="h-8" />
       <ConnectButton />
     </header>
   );
