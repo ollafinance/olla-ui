c0424782a7eaea3db4c21b4da61f1c183628d898
filip <filip.harald@gmail.com>
2026-02-04 14:14:29 +0100

    done

--- src/providers/wagmi-provider.tsx
+++ src/providers/wagmi-provider.tsx
@@ -8,29 +8,38 @@ import {
   walletConnectWallet,
 } from "@rainbow-me/rainbowkit/wallets";
 import { foundry } from "viem/chains";
-import { http, WagmiProvider as WagmiProviderLib } from "wagmi";
+import { createConfig, http, WagmiProvider as WagmiProviderLib } from "wagmi";
+import { injected } from "wagmi/connectors";
 
-const config = getDefaultConfig({
-  appName: "Olla Finance",
-  appDescription: "Liquid Staking Protocole",
-  appUrl: "https://olla.finance",
-  appIcon: "https://olla.finance/logo.png",
-  projectId: WALLET_CONNECT_PROJECT_ID,
-  chains: [foundry],
-  transports: {
-    [foundry.id]: http(RPC_URL),
-  },
-  wallets: [
-    {
-      groupName: "Popular",
-      wallets: [metaMaskWallet, walletConnectWallet],
-    },
-    {
-      groupName: "More",
-      wallets: [trustWallet, ledgerWallet, safeWallet],
-    },
-  ],
-});
+const config = WALLET_CONNECT_PROJECT_ID
+  ? getDefaultConfig({
+      appName: "Olla Finance",
+      appDescription: "Liquid Staking Protocole",
+      appUrl: "https://olla.finance",
+      appIcon: "https://olla.finance/logo.png",
+      projectId: WALLET_CONNECT_PROJECT_ID,
+      chains: [foundry],
+      transports: {
+        [foundry.id]: http(RPC_URL),
+      },
+      wallets: [
+        {
+          groupName: "Popular",
+          wallets: [metaMaskWallet, walletConnectWallet],
+        },
+        {
+          groupName: "More",
+          wallets: [trustWallet, ledgerWallet, safeWallet],
+        },
+      ],
+    })
+  : createConfig({
+      chains: [foundry],
+      transports: {
+        [foundry.id]: http(RPC_URL),
+      },
+      connectors: [injected()],
+    });
 
 export function WagmiProvider({ children }: { children: React.ReactNode }) {
   return <WagmiProviderLib config={config}>{children}</WagmiProviderLib>;
