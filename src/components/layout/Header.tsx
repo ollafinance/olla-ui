5627515c792045644932afa2deabec6140ef189c
Mauro Eijsenring <mauro@eijsenring.com>
2026-02-10 13:55:07 +0100

    add logic for network selection

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -39,7 +39,7 @@ export function Header() {
 
       <div className="flex items-center justify-end gap-3 min-w-[150px]">
         <ThemeToggle />
-        <ConnectButton />
+        <ConnectButton chainStatus="full" showBalance={false} />
       </div>
     </header>
   );
