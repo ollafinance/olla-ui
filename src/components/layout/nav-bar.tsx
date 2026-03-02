e5a266b4bc7972c461bd45dc76defb639add3e54
Mauro Eijsenring <mauro@eijsenring.com>
2026-03-02 17:14:11 -0600

    fix colors card and background

--- src/components/layout/nav-bar.tsx
+++ src/components/layout/nav-bar.tsx
@@ -2,16 +2,16 @@ import { Link } from "@tanstack/react-router";
 
 export function NavBar() {
   return (
-    <nav className="bg-surface border-surface-border flex items-center gap-2 rounded-full border p-2">
+    <nav className="bg-card flex items-center gap-2 rounded-full border border-0 p-2">
       <Link
         to="/stake"
-        className="bg-surface text-surface-foreground [&.active]:bg-card rounded-[56px] px-4 py-3 text-lg leading-[1.16] font-medium tracking-[-0.36px] transition-all [&.active]:text-black"
+        className="bg-card text-foreground [&.active]:bg-muted-soft [&.active]:text-foreground rounded-[56px] px-4 py-3 text-lg leading-[1.16] font-medium tracking-[-0.36px] transition-all"
       >
         Stake
       </Link>
       <Link
         to="/redeem"
-        className="bg-surface text-surface-foreground [&.active]:bg-card rounded-[56px] px-4 py-3 text-lg leading-[1.16] font-medium tracking-[-0.36px] transition-all [&.active]:text-black"
+        className="bg-card text-foreground [&.active]:bg-muted-soft [&.active]:text-foreground rounded-[56px] px-4 py-3 text-lg leading-[1.16] font-medium tracking-[-0.36px] transition-all"
       >
         Redeem
       </Link>
