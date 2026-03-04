ac996064355fff58b169326feb68a0b6269a412d
Mauro Eijsenring <mauro@eijsenring.com>
2026-03-03 22:48:54 -0800

    udpate claim item

--- src/features/redeem/components/cards/WithdrawalCardPending.tsx
+++ src/features/redeem/components/cards/WithdrawalCardPending.tsx
@@ -5,30 +5,38 @@ interface WithdrawalCardPendingProps {
 }
 
 export function WithdrawalCardPending({ state }: WithdrawalCardPendingProps) {
-  const title = {
-    signing: "Signing...",
-    pending: "Transaction",
-    confirming: "Confirming",
-  }[state];
-
-  const subtitle = {
-    signing: "Please sign in your wallet",
-    pending: "Submitted...",
-    confirming: "Awaiting confirmation...",
+  const content = {
+    signing: (
+      <>
+        <p className="mb-0">Sign</p>
+        <p>Transaction...</p>
+      </>
+    ),
+    pending: (
+      <>
+        <p className="mb-0">Transaction</p>
+        <p>Submitted...</p>
+      </>
+    ),
+    confirming: (
+      <>
+        <p className="mb-0">Transaction</p>
+        <p>Confirming...</p>
+      </>
+    ),
   }[state];
 
   return (
     <div className="bg-card rounded-card flex h-full min-h-[551px] w-full flex-col p-8">
-      <div className="text-[67px] leading-none font-medium tracking-[-1.35px] whitespace-nowrap text-black">
-        <p className="mb-0">{title}</p>
-        <p>{subtitle}</p>
+      <div className="text-text-display text-[67.43px] leading-none font-medium tracking-[-1.35px] whitespace-nowrap">
+        {content}
       </div>
 
-      <div className="mt-[22px]">
+      <div className="mt-[89px]">
         <img src={ollaLoading} alt="Loading" className="h-[59px] w-[128px] animate-pulse" />
       </div>
 
-      <div className="bg-secondary-accent mt-[31px] h-px w-full max-w-[487px]" />
+      <div className="bg-primary-line mt-[-0.5px] h-px w-full max-w-[487px]" />
 
       <div className="flex-1" />
     </div>
